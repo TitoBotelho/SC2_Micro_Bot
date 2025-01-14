fe13d56923930f5ae4c75e1d74f6d16d13536ed6
TitoBotelho <103445424+TitoBotelho@users.noreply.github.com>
2025-01-14 11:53:31 -0300

    fix the zergling combat manouver

--- bot/main.py
+++ bot/main.py
@@ -104,12 +104,13 @@ class MyBot(AresBot):
             target = self.game_info.map_center
 
 
+
         if self.roach_squad:
             self.roach_army_attack(self.roach_squad)
 
 
         if self.zergling_squad:
-            self.zergling_army_attack(self.zergling_squad, target, ground_grid)    
+            self.zergling_army_attack(self.zergling_squad)    
 
 
 
@@ -183,6 +184,12 @@ class MyBot(AresBot):
                 tag=unit.tag, role=UnitRole.CONTROL_GROUP_TWO
             )
 
+        if unit.type_id == UnitTypeId.HYDRALISK:
+            self.mediator.assign_role(
+                tag=unit.tag, role=UnitRole.CONTROL_GROUP_TWO
+            )
+
+
 
         if unit.type_id == UnitTypeId.QUEEN:
             self.mediator.assign_role(
@@ -243,12 +250,59 @@ class MyBot(AresBot):
 #_______________________________________________________________________________________________________________________
 
 
-    def zergling_army_attack(self, main_attack_force: Units, attack_target: Point2, ground_grid: np.ndarray) -> None:    
+    def zergling_army_attack(self, main_attack_force: Units) -> None:    
+
+        query_type: UnitTreeQueryType = (
+            UnitTreeQueryType.EnemyGround
+            if self.race == Race.Zerg
+            else UnitTreeQueryType.AllEnemy
+        )
+        near_enemy: dict[int, Units] = self.mediator.get_units_in_range(
+            start_points=main_attack_force,
+            distances=15,
+            query_tree=query_type,
+            return_as_dict=True,
+        )
+
+        # get a ground grid to path on, this already contains enemy influence
+        grid: np.ndarray = self.mediator.get_ground_grid
+        
+
+        # make a single call to self.attack_target property
+        # otherwise it keep calculating for every unit
+        target: Point2 = self.attack_target
+
+        # use `ares-sc2` combat maneuver system
+        # https://aressc2.github.io/ares-sc2/api_reference/behaviors/combat_behaviors.html
         for unit in main_attack_force:
-            if unit.is_idle:  # Verifica se a unidade está ociosa antes de dar o comando
-                main_maneuver = CombatManeuver()
-                main_maneuver.add(AMove(unit, attack_target))
-                self.register_behavior(main_maneuver)
+            """
+            Set up a new CombatManeuver, idea here is to orchestrate your micro
+            by stacking behaviors in order of priority. If a behavior executes
+            then all other behaviors will be ignored for this step.
+            """
+
+            attacking_maneuver: CombatManeuver = CombatManeuver()
+            # we already calculated close enemies, use unit tag to retrieve them
+            all_close: Units = near_enemy[unit.tag].filter(
+                lambda u: not u.is_memory and u.type_id not in COMMON_UNIT_IGNORE_TYPES
+            )
+            only_enemy_units: Units = all_close.filter(
+                lambda u: u.type_id not in ALL_STRUCTURES
+            )
+
+            # enemy around, engagement control
+            if all_close:
+                attacking_maneuver.add(AMove(unit=unit, target=target))
+
+
+                #attacking_maneuver.add(KeepUnitSafe(unit=unit, grid=grid))
+
+            # no enemy around, path to the attack target
+            else:
+                attacking_maneuver.add(AMove(unit=unit, target=target))
+
+            # DON'T FORGET TO REGISTER OUR COMBAT MANEUVER!!
+            self.register_behavior(attacking_maneuver)
 
 
     def roach_army_attack(self, main_attack_force: Units) -> None:
